4d110ec51c7eb541206d6c858b772ecf4d2f4f9f
YaroslavGnatyuk <yaroslavforjob@gmail.com>
2017-03-08 11:19:41 +0200

    I added repository classes for all entities.

--- src/main/java/ua/in/gnatyuk/repository/CurrentRentRepository.java
+++ src/main/java/ua/in/gnatyuk/repository/CurrentRentRepository.java
@@ -3,8 +3,8 @@ package ua.in.gnatyuk.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import ua.in.gnatyuk.entity.HotelRoom;
+import ua.in.gnatyuk.entity.CurrentRent;
 
 @Transactional(propagation = Propagation.MANDATORY)
-public interface HotelRoomRepository extends JpaRepository<HotelRoom, Long> {
+public interface CurrentRentRepository extends JpaRepository<CurrentRent, Long>{
 }
--- src/main/java/ua/in/gnatyuk/repository/HotelRoomReservationRepository.java
+++ src/main/java/ua/in/gnatyuk/repository/HotelRoomReservationRepository.java
@@ -3,8 +3,9 @@ package ua.in.gnatyuk.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import ua.in.gnatyuk.entity.HotelRoom;
+import ua.in.gnatyuk.entity.HotelRoomReservation;
 
 @Transactional(propagation = Propagation.MANDATORY)
-public interface HotelRoomRepository extends JpaRepository<HotelRoom, Long> {
+public interface HotelRoomReservationRepository extends JpaRepository<HotelRoomReservation, Long>{
+
 }
